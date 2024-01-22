38bc5f4100058a872a5e50dcc00643f1a9836ab3
Biediger <cabiediger@gmail.com>
2024-01-22 13:54:23 +0800

    update nav

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,70 +1,21 @@
-import { Fragment, useState } from "react";
 import { Outlet } from 'react-router-dom';
 import App from '../App';
-import logo from '../assets/logo.png'
+import { Navbar } from "../components/navbar/navbar.component";
+
 import './root.css'
 
 const Root = () => {
-const [ toggleNav, setToggleNav ] = useState(false) 
-
-const handleToggleNav = () => {
-    setToggleNav(!toggleNav);
-    console.log(toggleNav)
-}
-
-return (
-<Fragment>
-    <div id="sidebar" className={toggleNav ? "animate" : "animate left"}>
-    <div className="nav-logo-name">
-        <img src={logo} alt="food logo" className="nav-logo" />
-        <h3>Home Cooked</h3>
-        <div className="menu cross menu--1" >
-            <label>
-            <input type="checkbox" autoComplete="off" onClick={() => handleToggleNav()}/>
-            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="50" cy="50" r="30" />
-                <path className="line--1" d="M0 40h62c13 0 6 28-4 18L35 35" />
-                <path className="line--2" d="M0 50h70" />
-                <path className="line--3" d="M0 60h62c13 0 6-28-4-18L35 65" />
-            </svg>
-            </label>
+    return (
+    <div className="flex flex-col">
+        <Navbar />
+        <div id="detail">
+        {window.location.pathname === '/' ? 
+            <App /> :
+            <Outlet />
+        }
         </div>
     </div>
-    <nav className="navbar ">
-        <ul className={!toggleNav ? "links links-closed" : "links links-open"}>
-        <li>
-            <a href={`/`}>Home</a>
-        </li>
-        <li>
-            <a href={`/signup`}>SOME LINK HERE</a>
-        </li>
-        <li>
-            <a href={`/authentication`}>Login/Sign up</a>
-        </li>
-        </ul>
-        <div className={!toggleNav ? "name-container name-in" : "name-container name-out" }>
-        <span>H</span>
-        <span>O</span>
-        <span>M</span>
-        <span>E</span>
-        <br/>
-        <span>C</span>
-        <span>O</span>
-        <span>O</span>
-        <span>K</span>
-        <span>E</span>
-        <span>D</span>
-        </div>
-    </nav>
-    </div>
-    <div id="detail">
-    {window.location.pathname === '/' ? 
-        <App /> :
-        <Outlet />
-    }
-    </div>
-</Fragment>
-);
+    );
 }
 
 export default Root;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   content: [
@@ -8,8 +9,26 @@ module.exports = {
       "./node_modules/tw-elements-react/dist/js/**/*.js"
   ],
   theme: {
-      extend: {},
+      extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
   darkMode: "class",
   plugins: [require("tw-elements-react/dist/plugin.cjs")]
   }
