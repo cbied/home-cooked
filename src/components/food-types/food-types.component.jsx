825832045039e4adf3fd6626a86d31859e7705d9
Biediger <cabiediger@gmail.com>
2024-01-28 15:58:21 +0800

    add food types selection in nav, clean up nav, add mock data for food types

---
+++ src/components/food-types/food-types.component.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react"
+import { MultiCascader, Button } from 'rsuite';
+import mockFoodData from '../../mockData/mockFoodTypes.json'
+
+const styles = { width: 250, display: 'block', marginBottom: 10 };
+
+const FoodTypes = () => {
+
+    return (
+        <Fragment>
+            <MultiCascader toggleAs={Button} size="lg"  placeholder="Type of Food" data={mockFoodData} style={styles} />
+        </Fragment>
+    )
+}
+
+export default FoodTypes;
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -9,24 +9,14 @@ import {
   TEDropdownItem,
   TERipple,
 } from "tw-elements-react";
+import FoodTypes from '../food-types/food-types.component'
 import { signOutUser } from '../../utils/firebase.utils';
 import logo from '../../assets/logo.png'
 import './navbar.styles.css';
 
 export const Navbar = () => {
-    const [ toggleNav, setToggleNav ] = useState(false);
     const dispatch = useDispatch()
     const selectCurrentUser = useSelector(state => state.user.currentUser);
-
-    const handleToggleNav = () => {
-        setToggleNav(!toggleNav);
-
-        setTimeout(() => {
-            if(toggleNav) {
-                window.document.getElementById('navbar').className += ' hide'
-            }
-        }, 900)
-    }
 
     async function handleSignUserOut() {
         return signOutUser().then(() => {
@@ -34,67 +24,43 @@ export const Navbar = () => {
         })
       
     }
-
-    useEffect(() => {
-    const hasHide = window.document.getElementById('navbar').classList.contains('hide');
-    if(!hasHide) {
-        window.document.getElementById('navbar').className += ' hide'
-        }
-    }, [])
     
 return(
     <nav>
         <div className='flex justify-between w-screen'>
             <div>
-                <div className="menu cross menu--1 hamburger">
-                    <label className='h-20'>
-                    <input type="checkbox" autoComplete="off"/>
-                    <svg viewBox="0 0 170 170" xmlns="http://www.w3.org/2000/svg" onClick={handleToggleNav}>
-                        <circle cx="50" cy="50" r="30" />
-                        <path className="line--1" d="M0 40h62c13 0 6 28-4 18L35 35" />
-                        <path className="line--2" d="M0 50h70" />
-                        <path className="line--3" d="M0 60h62c13 0 6-28-4-18L35 65" />
-                    </svg>
-                    </label>
-                </div>
-                <div className='hidden md:block h-16 pl-15'>
+                
+                <a className='block h-16 pl-15 my-5' href={'/'}>
                     <img className='w-16 h-12' src={logo} alt="home cooked logo" />
-                </div>
+                </a>
             </div>
 
             <div className='hidden md:flex md:items-center'>
                 <Nav className='flex justify-around items-center'>
-                    <Nav.Item href={'/'}>Home</Nav.Item>
-                    <Nav.Item href={'/auth'}>Login/Sign up</Nav.Item>
-                    <Nav.Item>Solutions</Nav.Item>
-                    <Nav.Item>Products</Nav.Item>
-                    <Nav.Item>About</Nav.Item>
+                    <Nav.Item href={'/'}>Be a Guest</Nav.Item>
+                    <Nav.Item href={'/'}>Learn about the experience</Nav.Item>
+                    <Nav.Item href={'/'}>Be a Host</Nav.Item>
                 </Nav>
             </div>
 
             <div className='flex self-center pr-21 h-16'>
                 <TEDropdown className="flex justify-center">
                 
-                {
-                selectCurrentUser ? 
                 <TERipple rippleColor="light">
                     <TEDropdownToggle className="flex items-center whitespace-nowrap px-5 pt-2 rounded text-xs font-medium uppercase leading-normal text-white ">
                     <span className="ml-2 w-12">
                         <img
-                        src={selectCurrentUser.photoURL ? selectCurrentUser.photoURL : "https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
+                        src={selectCurrentUser && selectCurrentUser.photoURL ? selectCurrentUser.photoURL : "https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
                         className="w-16 rounded-full"
                         alt="Avatar" />
                     </span>
                     </TEDropdownToggle>
                 </TERipple>
-                :
-                <div></div>
-                }
 
                 <TEDropdownMenu>
                     <TEDropdownItem>
                     <a href="/" className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600">
-                        Action
+                        Dashboard
                     </a>
                     </TEDropdownItem>
                     <TEDropdownItem>
@@ -104,27 +70,30 @@ return(
                     </TEDropdownItem>
                     <TEDropdownItem>
                     <a href="/" className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600">
-                        Something else here
+                        Settings
                     </a>
                     </TEDropdownItem>
                     <hr className="my-2 h-0 border border-t-0 border-solid border-neutral-700 opacity-25 dark:border-neutral-200" />
-                    <TEDropdownItem onClick={handleSignUserOut}>
-                    <a href="/" className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600">
-                        Sign out
-                    </a>
+                    <TEDropdownItem >
+                    <div  className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600">
+                        {selectCurrentUser ?
+                            <a href="/" onClick={handleSignUserOut} alt="sign out"
+                            className='font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600'>Sign Out</a>
+                            : 
+                            <a href={"/auth"}>
+                            Login
+                            </a>
+                        }
+                    </div>
                     </TEDropdownItem>
                 </TEDropdownMenu>
                 </TEDropdown>
             </div>
         </div>
-
-        <Nav id="navbar" className={toggleNav ? 'down' : 'up'}>
-            <Nav.Item href={'/'}>Home</Nav.Item>
-            <Nav.Item href={'/auth'}>Login/Sign up</Nav.Item>
-            <Nav.Item>Solutions</Nav.Item>
-            <Nav.Item>Products</Nav.Item>
-            <Nav.Item>About</Nav.Item>
-        </Nav>
+        <div className='flex justify-center'>
+            <FoodTypes />
+            
+        </div>
 
     </nav>
 )
