5ad0bbd6198530a71cd342ab7b0acdbd5d2dfea6
Biediger <cabiediger@gmail.com>
2024-02-04 12:08:31 +0800

    update projects to be able to use light and dark mode with rsuite

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,17 @@
+const CracoLessPlugin = require('craco-less');
+
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-}
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#f44336' },
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ]
+};
