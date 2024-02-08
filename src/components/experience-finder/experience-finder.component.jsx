b7151ca3415859f8347b6ea206ee541b7e080731
Biediger <cabiediger@gmail.com>
2024-02-08 11:12:14 +0800

    update experience finder to have dividers, update inputs borders in experience finder, fix color of button

--- src/components/experience-finder/experience-finder.component.jsx
+++ src/components/experience-finder/experience-finder.component.jsx
@@ -1,19 +1,21 @@
 import FoodTypes from "../food-types/food-types.component";
 import DatePicker from "../date-range-picker/date-range-picker.component";
 import PartySizeInput from "../party-size-input/party-size-input.component";
-import { IconButton, ButtonToolbar } from 'rsuite';
+import { IconButton, ButtonToolbar, Divider } from 'rsuite';
 import SearchIcon from '@rsuite/icons/Search';
 
 
 
 const ExperienceFinder = () => {
 
-
+    const styles = { height: 'auto' }
 
     return (
-        <div className="w-fit flex self-center outline outline-orange-300 outline-1 rounded-full mb-3 overflow-hidden">
+        <div className="w-fit flex self-center outline outline-1 rounded-full mb-3 overflow-hidden">
             <FoodTypes />
+            <Divider vertical style={styles}/>
             <DatePicker />
+            <Divider vertical style={styles}/>
             <PartySizeInput />
             <ButtonToolbar>
                 <IconButton size="lg" icon={<SearchIcon />} />
