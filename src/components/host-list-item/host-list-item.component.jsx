d6c1a2365d80fbc068798820812dcbaac42fdd79
Biediger <cabiediger@gmail.com>
2024-03-28 08:55:51 +1300

    only list dinners that are within search radius within the host list

--- src/components/host-list-item/host-list-item.component.jsx
+++ src/components/host-list-item/host-list-item.component.jsx
@@ -1,39 +1,49 @@
 import { Panel, Stack, ButtonGroup, Button } from "rsuite";
+import {
+  check,
+  geoPosition,
+  meter,
+} from "../../utils/google-maps/google-maps.utils";
 import { hostMarkers } from "../../mockData/mockHostMakers";
 
 const HostListItem = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       {hostMarkers.map((host) => {
-        return (
-          <Panel
-            key={host.uid}
-            className="w-full md:w-3/4 my-5 md:px-5"
-            bordered
-            header={
-              <Stack className="w-full flex justify-between">
-                <div>
-                  <h3>
-                    {host.foodType} - {host.hostDisplayName}
-                  </h3>
-                  <h5>Guest spots left: {host.guestSpotsLeft}</h5>
-                </div>
-                <ButtonGroup>
-                  <Button appearance="primary">Register</Button>
-                </ButtonGroup>
-              </Stack>
-            }
-          >
-            <div className="flex">
-              <img
-                className="w-24 h-24 mr-8 rounded-full"
-                src={host.image}
-                alt={host.foodType}
-              />
-              <p>{host.description}</p>
-            </div>
-          </Panel>
-        );
+        const markerPosition = { lat: host.lat, lng: host.lng };
+        if (check(markerPosition, geoPosition, meter)) {
+          return (
+            <Panel
+              key={host.uid}
+              className="w-full md:w-3/4 my-5 md:px-5"
+              bordered
+              header={
+                <Stack className="w-full flex justify-between">
+                  <div>
+                    <h3>
+                      {host.foodType} - {host.hostDisplayName}
+                    </h3>
+                    <h5>Guest spots left: {host.guestSpotsLeft}</h5>
+                  </div>
+                  <ButtonGroup>
+                    <Button appearance="primary">Register</Button>
+                  </ButtonGroup>
+                </Stack>
+              }
+            >
+              <div className="flex">
+                <img
+                  className="w-24 h-24 mr-8 rounded-full"
+                  src={host.image}
+                  alt={host.foodType}
+                />
+                <p>{host.description}</p>
+              </div>
+            </Panel>
+          );
+        } else {
+          return null;
+        }
       })}
     </div>
   );
