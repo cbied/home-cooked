892ffc1342e34f50d4f259cd5f1fbe4309afc059
Biediger <cabiediger@gmail.com>
2024-04-19 11:40:34 -0400

    update admin side nav based on screen width

--- src/components/side-nav/side-admin-nav.component.jsx
+++ src/components/side-nav/side-admin-nav.component.jsx
@@ -1,37 +1,26 @@
-import { useState } from "react";
-import { Sidenav, Nav, Toggle, Sidebar } from "rsuite";
+import React, { useState } from "react";
+import { Sidenav, Nav, Sidebar } from "rsuite";
 import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
 import GroupIcon from "@rsuite/icons/legacy/Group";
 import MagicIcon from "@rsuite/icons/legacy/Magic";
 import GearCircleIcon from "@rsuite/icons/legacy/GearCircle";
 
-const SideAdminNav = ({ expanded, setExpanded, navBodyStyle }) => {
+const SideAdminNav = ({ screenSize }) => {
   const [activeKey, setActiveKey] = useState("1");
-
-  const setNavExpanded = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <div style={{ width: 240, height: "100%" }}>
-      <Toggle
-        onChange={setNavExpanded}
-        checked={expanded}
-        checkedChildren="Expand"
-        unCheckedChildren="Collapse"
-      />
-      <hr />
       <Sidebar
         style={{ display: "flex", flexDirection: "column", height: "100%" }}
-        width={expanded ? 260 : 56}
+        width={screenSize[0] > 650 ? 260 : 56}
         collapsible
       >
         <Sidenav
-          expanded={expanded}
+          expanded={screenSize[0] > 650 ? true : false}
           defaultOpenKeys={["3", "4"]}
           style={{ height: "100%" }}
         >
-          <Sidenav.Body style={navBodyStyle}>
+          <Sidenav.Body style={screenSize[0] > 650 ? { overflow: "auto" } : {}}>
             <Nav activeKey={activeKey} onSelect={setActiveKey}>
               <Nav.Item eventKey="1" icon={<DashboardIcon />}>
                 Dashboard
@@ -43,6 +32,7 @@ const SideAdminNav = ({ expanded, setExpanded, navBodyStyle }) => {
                 placement="rightStart"
                 eventKey="3"
                 title="Advanced"
+                trigger="hover"
                 icon={<MagicIcon />}
               >
                 <Nav.Item eventKey="3-1">Geo</Nav.Item>
@@ -54,6 +44,7 @@ const SideAdminNav = ({ expanded, setExpanded, navBodyStyle }) => {
                 placement="rightStart"
                 eventKey="4"
                 title="Settings"
+                trigger="hover"
                 icon={<GearCircleIcon />}
               >
                 <Nav.Item eventKey="4-1">Applications</Nav.Item>
@@ -66,7 +57,6 @@ const SideAdminNav = ({ expanded, setExpanded, navBodyStyle }) => {
               </Nav.Menu>
             </Nav>
           </Sidenav.Body>
-          <Sidenav.Toggle onToggle={(expanded) => setExpanded(expanded)} />
         </Sidenav>
       </Sidebar>
     </div>
